8173f422fe5385371fe454fe56092256e66c9af8
ZeroIce <zeroicegc@163.com>
2021-03-06 18:53:33 +0800

    添加新闻模块

--- Office/miniprogram/pages/news/news.js
+++ Office/miniprogram/pages/news/news.js
@@ -1,66 +1,13 @@
-// pages/news/news.js
+var WxParse = require('../../wxParse/wxParse.js');
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    title:'代表委员呼吁：加强网络游戏监管，拯救网瘾少年',
+    from:'中国教育报',
+    time:'2021-03-05 19:44:23'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad: function () {
+    var that = this;
+    var article = `<article class="syl-page-article syl-device-pc tt-article-content"><div class="pgc-img"><img src="https://p6-tt.byteimg.com/origin/pgc-image/8716c57443b84bf39d12f7181c19e5d7?from=pc" img_width="1920" img_height="316" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>步入职场后，最让你们头疼的是什么？</p><p>要我说，一定是这件事情！</p><p>好不容易熬到假期，约好小姐妹出门逛街，路上突然领导发来消息，要求审核几份紧急文件：</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/445aa394a36d4f56b6975e1a72a7dfc2.png?from=pc" img_width="584" img_height="581" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>强烈的求生欲使我不得不掏出 WPS Office，争分夺秒地打开各个文件。</p><div class="pgc-img"><img src="https://p6-tt.byteimg.com/origin/pgc-image/8ee01f80e2034f37a46a414e43f90677.png?from=pc" img_width="403" img_height="693" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption">毕竟，我只是一个卑微的打工仔</p></div><p>但是接下来发生的一些事情，超乎了我的意料：</p><p>开屏界面，等待 3 秒，点击跳过，反而进入广告；</p><p>首页文档，隐藏广告，不小心误触，给原本并不富裕的时间「雪上加霜」。</p><div class="pgc-img"><img src="https://p1-tt.byteimg.com/origin/pgc-image/9cf3572a93ae48c88aca3277146138b0.png?from=pc" img_width="801" img_height="722" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption">手指粗的人真的很容易点进广告，555~</p></div><p>有没有一款无广告 APP，让我可以轻松办公啊？o(╥﹏╥)o</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/eb1cc46908a84b549d39f74d81f39d9d.png?from=pc" img_width="430" img_height="474" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p class="syl-page-br syl-page-br-hide"><br></p><p>我原本可以很快乐，都是烦楞的工作广告害了我。</p><p class="syl-page-br"><br></p><p>终于，我挖掘到了一款办公全家桶——Office app ：</p><p class="syl-page-br syl-page-br-hide"><br></p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/5d62a65852ff4190a713518c4cb6feb8.png?from=pc" img_width="270" img_height="256" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p class="syl-page-br syl-page-br-hide"><br></p><p>花了几天时间体验后，发现这真是一个随身办公利器：</p><p><strong>界面简洁大方，功能尽收眼底，满足 Word、Excel、PPT 基础操作需求~</strong></p><p>最戳中我内心的是——<strong>没有广告！！！</strong></p><p>除此之外，还有一些非常实用的功能，帮我解决了好多工作中的难题！</p><p>现在就来和大家分享啦~</p><p class="syl-page-br syl-page-br-hide"><br></p><div class="pgc-img"><img src="https://p1-tt.byteimg.com/origin/pgc-image/634413b508f54e10ab337e31cdcaa43c.png?from=pc" img_width="661" img_height="621" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/75d854a4e113443291be72ecd07e12e7?from=pc" img_width="750" img_height="264" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><h1 class="pgc-h-arrow-right">直接录入，格式不用转来转去</h1><p>大家一定会遇到这样的情况：</p><blockquote><p>工作交接的时候，同事给了一份纸质材料，要你把它变成可编辑的 Word 文档。</p></blockquote><p>你不会还准备打字录入吧？优秀的人早就已经学会了这个功能——<strong>扫描录入。</strong></p><p>在 WPS Office 里，使用拍照扫描功能，只能生成 PDF 文件或图片，如果想将图片转 Word 文档，或者提取图片中的文字，就需要开通会员。</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/2fd30a240113437394453b66a64357ee.png?from=pc" img_width="1080" img_height="615" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>但在 Office app，这个功能不收取任何费用！</p><p><strong>步骤非常简单：</strong></p><p><strong>Step1：</strong>打开 Office app，点击下方操作栏中间的【+】，打开【文档】；</p><div class="pgc-img"><img src="https://p6-tt.byteimg.com/origin/pgc-image/ed36362227544b869761dfe5e4f9a571.png?from=pc" img_width="399" img_height="247" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p><strong>Step 2：</strong>选择【扫描文本】/【扫描表】（一次可以扫描多张图片哦）；</p><div class="pgc-img"><img src="https://p6-tt.byteimg.com/origin/pgc-image/f76ffe80dc9348149e8f0f9c94e6938c.png?from=pc" img_width="581" img_height="265" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p><strong>Step 3：</strong>拍照，点击【完成】，即可识别录入。</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/68acf430203849a5adb85b7494d1f8f9.png?from=pc" img_width="811" img_height="699" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption">识别文本</p></div><p>识别出的文字会自动显示在文档中，<strong>准确度非常高~</strong></p><p>表格也可以进行识别，虽然精准度没有文本扫描高，但大体上都能正常显示，只需简单改一改。</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/08faeea29f15498e801c00a684452ead.png?from=pc" img_width="813" img_height="696" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption">识别表格</p></div><p>虽然 Office app 识别的速度稍微慢了一些，但是扛不住它免费呀！你值得拥有~</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/4695a297d5d341f59f8dfe1756719006?from=pc" img_width="750" img_height="264" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><h1 class="pgc-h-arrow-right">语音听写，记录重要事项</h1><p>日常开会的时候，boss 布置了很多工作内容，来不及记录下来怎么办？</p><p>这时候，Office app 的「<strong>语音听写功能</strong>」就能派上大用处了，语音迅速识别转换成文字~</p><p>主打「语音听写」的讯飞语记，免费转换会限制时长！</p><p style="text-align: left;">If you want more, you need to pay</p><div class="pgc-img"><img src="https://p1-tt.byteimg.com/origin/pgc-image/182f2070a0bc42de9037d6ea90470153.png?from=pc" img_width="417" img_height="625" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>但在 Office app 中，这个功能不仅准确，而且还免费！</p><p>只需简单几步就能够完成转换：</p><p><strong>step 1：</strong>打开【文档】-【空白文档】</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/70e38a0320ed4679ab8b3788fc29ff12.png?from=pc" img_width="462" img_height="219" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p class="syl-page-br syl-page-br-hide"><br></p><p><strong>step 2：</strong>点击话筒图标，开始语音录入</p><div class="pgc-img"><img src="https://p1-tt.byteimg.com/origin/pgc-image/f272b40140c147e3825764038da290b1.png?from=pc" img_width="415" img_height="391" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p class="syl-page-br syl-page-br-hide"><br></p><p>这个功能有两个最能戳到我的点：</p><p><strong>一是自动添加标点符号：</strong></p><p>根据语气变化，自动添加标点符号，拯救断句困难患者。</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/736857412e8e4b71959ae1c0769f0621?from=pc" img_width="640" img_height="1066" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p><strong>二是多语言录入：</strong></p><p>除了中文，还有德文、意大利文、法文、英文、西班牙文这 5 种语言可以录入。</p><p>即使谈话对象说外语，你也能轻松记录~</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/3d5a630ae4e0474bbf1e1270488f56aa?from=pc" img_width="639" img_height="1024" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>这个功能精准实现语音转文字，采访、备忘、会议记录......都可以用。</p><p>在这个能喊话就不打字的时代，的确是办公一大杀器！</p><p>开会时，再也不用担心领导的话记不住了！偶尔灵感来了，也不怕打字速度配不上思维跳跃的跨度啦~</p><div class="pgc-img"><img src="https://p6-tt.byteimg.com/origin/pgc-image/497404ae72e34dad93deccfc3492307d?from=pc" img_width="750" img_height="264" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><h1 class="pgc-h-arrow-right">重复工作，选择模板创建</h1><p>大家工作中或多或少都会遇到需要套模板的情况，但是网上搜索的模板不仅耗时间，而且大多需要付费，为了一个临时任务开一个月的会员，多不划算呀~</p><p>这时候， Office app 模板库就能派上用场了！</p><p>点击【从模板创建】就能找到各种各样的模板</p><div class="pgc-img"><img src="https://p1-tt.byteimg.com/origin/pgc-image/2a4dde5bc9de415f9774025fe4405507.png?from=pc" img_width="385" img_height="540" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>每个模板都清楚标明了用途，共 71 个，涵盖了大部分使用场景，不仅实用而且免费！</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/04b41d1ffed148e1ad02e8c33b0b3615.png?from=pc" img_width="1080" img_height="627" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>相比而言，WPS 模板更加精致，但是需要开会员才可以使用~</p><div class="pgc-img"><img src="https://p6-tt.byteimg.com/origin/pgc-image/0f78ada77394479b999c5a1e7d06da56.png?from=pc" img_width="1080" img_height="591" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>我就问你，微软 Office app 自带的免费模板它不香吗？</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/ca62aadf66b2456fa0d4c01d36c762ab?from=pc" img_width="750" img_height="264" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><h1 class="pgc-h-arrow-right">文件传输，更加方便快捷</h1><p>都 2020 年了，你还在用 QQ、微信互传文件吗？</p><p>Office app 也是传输文件的一把好手！不用在电脑上登陆各种账号，也无需下载其它软件，打开网页即可传输。</p><p>30M 以下的文件可以借助 QR 码在电脑与手机间传输，无需第三方，三步搞定：</p><p><strong>Step 1：</strong>点击【操作】-【传输文件】；</p><p><strong>Step 2：</strong>选择【发送】或【接收】；</p><p><strong>Step 3：</strong>在电脑端打开，使用手机扫描 QR 码。</p><div class="pgc-img"><img src="https://p1-tt.byteimg.com/origin/pgc-image/ae71fa6e0f884d92bbfdde6cb73def28.png?from=pc" img_width="1080" img_height="568" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption">电脑端网址：https://transfer.Office.com/</p></div><p>这样设备就可以与该计算机配对，进行文件传输啦~</p><p class="syl-page-br syl-page-br-hide"><br></p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/cada32187ff247c7ae04dafac29f8bd0?from=pc" img_width="638" img_height="287" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption">电脑同步操作界面</p></div><p class="syl-page-br syl-page-br-hide"><br></p><p>如果需要传输的文件大于 30M 怎么办呢？</p><div class="pgc-img"><img src="https://p3-tt.byteimg.com/origin/pgc-image/ab8f1aa53ed745e0bdb33b7388af6cf2.png?from=pc" img_width="440" img_height="410" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>通过 Office app 直接登录访问网盘里面的文档，有 19 个云盘可供选择！</p><p>点击头像，打开【云位置】-【添加位置】，就可以添加第三方的云服务了~</p><div class="pgc-img"><img src="https://p6-tt.byteimg.com/origin/pgc-image/bcbc776f5c784400b73d54a90eb51138.png?from=pc" img_width="1080" img_height="451" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>多数云盘都是国外比较受欢迎的，大家可以选择常用的网盘进行文档同步。</p><p>普通用户推荐使用腾讯微云，365 用户可以使用 OneDrive 和 OneDrive for Business（可惜没有我最常用的百度网盘）~</p><div class="pgc-img"><img src="https://p1-tt.byteimg.com/origin/pgc-image/ee1b22fabeca4268a0b21b6d9cb24fc9.png?from=pc" img_width="149" img_height="145" alt="再见WPS！微软新出的免费Office软件，我一用就爱上了" inline="0" class="syl-page-img"><p class="pgc-img-caption"></p></div><p>关于 Office app 的功能就介绍到这里了，安卓用户可以在应用商城找到它，iOS 用户可以在 App Store 下载，感兴趣的小伙伴们，心动不如行动~</p></article>`;
+    WxParse.wxParse('article', 'html', article, that, 5);
   }
 })
